d355e5ed82b378389fa86a78cd67ac8df284eab1
HolidayMan <37782099+HolidayMan@users.noreply.github.com>
2019-02-15 22:37:44 +0200

    It works, but there is much bad code

--- client.py
+++ client.py
@@ -1,6 +1,34 @@
 import socket
 import threading
+import sys
+
+def sendmessage():
+	global sock
+	while True:
+		message = input('>>> ').encode('utf-8')
+		sock.sendall(message)
+
+if len(sys.argv) != 2:
+	print('[!] Incorrect use. Correct use is "python3 file.py ip:port"')
+else:
+	try:
+		ip = sys.argv[1].split(':')[0]
+		port = int(sys.argv[1].split(':')[1])
+	except SyntaxError:
+		print('[!] Incorrect use. Correct use is "python3 file.py ip:port"')
 
 sock = socket.socket()
-sock.connect(('localhost', 9043))
-input()
+username = input('Username: ').encode('utf-8')
+sock.connect((ip, port))
+sock.sendall(username)
+threading.Thread(target=sendmessage, daemon=True).start()
+while True:
+	try:
+		message = sock.recv(1024).decode('utf-8')
+		print('\b'*4, '{}\n>>> '.format(message), sep='', end='')
+		if message == 'Server turned off':
+			break
+	except:
+		sock.sendall('{} disconnected'.format(username.decode('utf-8')).encode('utf-8'))
+		break
+sock.close()
