316bec7f5831199a7a7d777ec2f2d86d23c7ae70
HolidayMan <37782099+HolidayMan@users.noreply.github.com>
2019-02-15 22:37:13 +0200

    It works, but there is much bad code

--- server.py
+++ server.py
@@ -1,26 +1,82 @@
 import socket
 import threading
+import sys
+
+
+def send_to_everybody(mess, addr, user):
+	for i in clients:
+		if i[1] == addr:
+			continue
+		message = '{}: {}'.format(user, mess).encode('utf-8')
+		i[0].send(message)
+
+
+def recieving(connection, address, username):
+	while True:
+		mess = connection.recv(1024).decode('utf-8')
+		try:
+			if mess.split()[1] == 'disconnected':
+				send_to_everybody(mess, address, 'Server')
+				print('\b'*4, mess+'\n>>> ', sep='', end='')
+				del clients[clients.index((connection, address, username))]
+				connection.close()
+				break
+		except IndexError:
+			pass
+		send_to_everybody(mess, address, username)
+
 
 
 def acc():
 	global sock
 	global clients
+	sock.listen(3)
 	while True:
-		print("Here")
 		conn, addr = sock.accept()
-		print(conn, addr)
-
+		username = conn.recv(1024).decode('utf-8')
+		mess = '{} connected\n>>> '.format(username)
+		print('\b'*4, mess, sep='', end='')
+		send_to_everybody(mess, addr, 'Server')
+		clients.append((conn, addr, username))
+		threading.Thread(target=recieving, args=(conn, addr, username), daemon=True).start()
 
 clients = []
 
 ip = socket.gethostbyname((socket.gethostname()))
-port = 9043
+if len(sys.argv) == 2:
+	port = int(sys.argv[1])
+else:
+	print('Incorrect use. Specify the port.')
+	exit()
 
-sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((ip, port))
-sock.listen(1)
 
 if __name__ == "__main__":
-	connecting_thread = threading.Thread(target=acc, daemon=False)
+	connecting_thread = threading.Thread(target=acc, daemon=True)
 	connecting_thread.start()
-	print("[*] Server started")
+	print('[*] Server started on {}:{}'.format(ip, port))
+	commands = ['exit', 'showclients']
+	while True:
+		command = input('>>> ')
+		if command in commands:
+			if command == 'exit':
+				print('[*] Stopping server')
+				for i in clients:
+					i[0].send('Server turned off'.encode('utf-8'))
+					i[0].close()
+				sock.close()
+				exit()
+			if command == 'showclients':
+				if len(clients) > 0:
+					for i in range(len(clients)):
+						if i == len(clients)-1:
+							print(clients[i][2])
+							continue
+						print(clients[i][2]+', ', end='')
+				else:
+					print('No clients connected')
+		elif command in '\n ':
+			pass
+		else:
+			print('Unknown command')
